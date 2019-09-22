19f32558dfdf78aee0c5f962e197d7e1031b7203
Colby <colby.s@modula.co>
2019-09-23 00:10:45 +0100

    update form to submit via AJAX

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '~/plugins/form-serialize.js', ssr: false }],
   /*
    ** Nuxt.js dev-modules
    */
--- pages/rsvp.vue
+++ pages/rsvp.vue
@@ -6,7 +6,13 @@
     <h2>
       let us know if you can join us and which events you'd like to attend
     </h2>
-    <form name="rsvp" netlify netlify-honeypot="its-a-trap">
+    <form
+      name="rsvp"
+      action="/"
+      netlify
+      netlify-honeypot="its-a-trap"
+      @submit="handleRsvpSubmit"
+    >
       <input name="its-a-trap" type="text" class="hidden" />
 
       <div>Names <input name="names" type="text" /></div>
@@ -14,15 +20,28 @@
       <div>
         Are you attending?
 
-        <input name="attending" type="radio" value="R.S.V.P" />
+        <!-- Netlify isn't very smart at guessing key/names for checkboxes and radios in forms, this will trick Netlify to use this value as the label for the field -->
+        <input name="attending" type="radio" value="R.S.V.P" class="hidden" />
 
         <label for="attending-yes">
-          <input id="attending-yes" name="attending" type="radio" value="Yes" />
+          <input
+            id="attending-yes"
+            v-model="rsvpFormAttending"
+            name="attending"
+            type="radio"
+            value="Yes"
+          />
           Yes
         </label>
 
         <label for="attending-no">
-          <input id="attending-no" name="attending" type="radio" value="No" />
+          <input
+            id="attending-no"
+            v-model="rsvpFormAttending"
+            name="attending"
+            type="radio"
+            value="No"
+          />
           No
         </label>
       </div>
@@ -30,7 +49,8 @@
       <div>
         Which events would you like to join?
 
-        <input name="events[]" type="checkbox" value="Events" />
+        <!-- Netlify isn't very smart at guessing key/names for checkboxes and radios in forms, this will trick Netlify to use this value as the label for the field -->
+        <input name="events[]" type="checkbox" value="Events" class="hidden" />
 
         <label for="events-pool-party">
           <input
@@ -74,14 +94,87 @@
       </div>
 
       <div>
-        <input type="submit" value="Send" />
+        <input
+          type="submit"
+          value="Send"
+          :disabled="rsvpFormSubmitting || rsvpFormResult.success"
+        />
+      </div>
+
+      <div>
+        <span :class="{ error: !rsvpFormResult.success }">
+          {{ rsvpFormResult.message }}
+        </span>
       </div>
     </form>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      rsvpForm: false,
+      rsvpFormAttending: null,
+      rsvpFormSubmitting: false,
+      rsvpFormResult: {}
+    }
+  },
+  mounted() {
+    this.rsvpForm = document.querySelector('form[name=rsvp]')
+  },
+  methods: {
+    handleRsvpSubmit(e) {
+      e.preventDefault()
+      this.rsvpFormSubmitting = true
+
+      this.sendRsvpSubmit().then((response) => {
+        this.rsvpFormSubmitting = false
+
+        if (Number(response.status) !== 200) {
+          this.rsvpFormResult = {
+            success: false,
+            message: 'Sorry, something went wrong, please try again later'
+          }
+        } else {
+          this.rsvpFormResult = {
+            success: true,
+            message:
+              'Thank you, ' +
+              (this.rsvpFormAttending === 'Yes'
+                ? 'we look forward to seeing you.'
+                : "we're sorry you can't make it.")
+          }
+        }
+      })
+    },
+    sendRsvpSubmit() {
+      return new Promise((resolve, reject) => {
+        fetch(this.rsvpForm.getAttribute('action'), {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: window.serialize(this.rsvpForm)
+        })
+          .then((response) => {
+            resolve(response)
+          })
+          .catch((err) => {
+            reject(err)
+          })
+      })
+    }
+  }
+}
+</script>
+
 <style>
 .hidden {
   display: none;
+}
+
+.error {
+  color: red;
 }
 </style>
