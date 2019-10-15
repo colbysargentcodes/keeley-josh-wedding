950ec287f85360a7f1a12b0e1e44254854c32942
Colby Sargent <colby@sargent.codes>
2019-10-15 21:33:30 +0100

    updated itinerary text

--- pages/itinerary.vue
+++ pages/itinerary.vue
@@ -23,8 +23,8 @@
           <p class="event-datetime">
             {{
               event.noTime
-                ? $nuxt.$moment(event.date).format('dddd, MMMM Do')
-                : $nuxt.$moment(event.date).format('dddd, MMMM Do, h:mma')
+                ? $nuxt.$moment(event.date).format('dddd, Do MMMM')
+                : $nuxt.$moment(event.date).format('dddd, Do MMMM, h:mma')
             }}
           </p>
         </div>
@@ -51,64 +51,67 @@ export default {
           code: 'stag',
           title: 'Stag',
           description:
-            'A celebration held for a man who is about to get married, attended only by men',
-          location: 'Mykanos',
+            '“The first rule of the stag party, is not to talk about the stag party.”\n' +
+            '- Josh',
+          location: 'Old Town, Mykanos',
           date: '2020-06-20 19:00'
         },
         {
           code: 'hen',
           title: 'Hen',
           description:
-            'A celebration held for a woman who is about to get married, attended only by women',
-          location: 'Mykanos',
-          date: '2020-06-21 16:00'
+            '“It’s time to drink champagne and dance on the table!”\n' +
+            '- Keeley',
+          location: 'Old Town, Mykanos',
+          date: '2020-06-21 19:00'
         },
         {
           code: 'boat-trip',
           title: 'Boat Trip',
-          description: 'A celebration held on a boat, attended only by anyone',
-          location: 'Paros',
+          description:
+            'Set sail with us on a traditional Greek Kaiki and explore the secluded coves and pristine beaches of the small Cycladic islands. Dive into the turquoise waters of the Aegean Sea or relax on deck and watch the dolphins swim by, all while enjoying cold refreshments and a selection of local delicacies freshly prepared onboard.',
+          location: 'Piso Livadi, Paros',
           date: '2020-06-24 10:00'
         },
         {
           code: 'welcome-drinks',
           title: 'Welcome Drinks',
           description:
-            'A celebration held at a welcoming, attended only by attendees',
-          location: 'Paros',
-          date: '2020-06-25 18:30'
+            'You’re all here and we couldn’t be happier! Join us for cocktails and canapes on the harbour of this picturesque fishing village, as we watch the sunset on our final night as ‘Mr & Miss’.',
+          location: 'Barbarossa Restaurant, Naousa, Paros',
+          date: '2020-06-25 18:00'
         },
         {
           code: 'wedding',
           title: 'Wedding',
           description:
-            'A celebration held people getting married, attended only by everyone',
-          location: 'Noussa, Paros',
+            'It’s time for us to say “I do” and for you to #MeetTheMcNamaras!',
+          location: 'Private Villa, Paros',
           date: '2020-06-26 17:00'
         },
         {
           code: 'beach-recovery',
           title: 'Beach Recovery',
           description:
-            'A celebration held on a beach, attended only by people with hangovers',
-          location: 'Santa Maria, Paros',
-          date: '2020-06-27 14:00'
+            'There’s no better cure for a hangover than a swim in the ocean, or so Josh says?! Let’s hit the beach and find out! We’ll made sure there’s a big lunch and a snoozeworthy sunbed at the water’s edge with your name on it – see you there!',
+          location: 'Santa Maria Beach, Paros',
+          date: '2020-06-27 12:00'
         },
         {
           code: 'pool-party',
-          title: 'Pool Party',
+          title: 'Farewell Pool Party',
           description:
-            'A celebration held in and around a swimming pool, attended only by swimmers',
-          location: 'Private Villa',
+            'We’re firing up the BBQ, turning on the tunes and inflating the unicorn to bid farewell to what will have no doubt been our best summer as husband and wife! Clothing optional. Swimwear compulsory. Because it’s not that kind of party.',
+          location: 'Private Villa, Paros',
           date: '2020-06-28 11:00'
         },
         {
           code: 'island-hop',
           title: 'Island Hop',
           description:
-            'A celebration held for more holiday time, attended only by people with a lot of spare holidays',
+            'The supermodel of the Greek islands is calling our name – and I think I can hear yours too?! Why not extend your trip for a few days and join us in Santorini before we jet off on our honeymoon? We’ve partied in Paros, now let’s conquer the caldera! No itinerary. No expectations. Just good times.',
           location: 'Santorini',
-          date: '2020-06-30',
+          date: '2020-06-30 13:00',
           noTime: true
         }
       ]
@@ -131,7 +134,7 @@ export default {
     display: inline-flex;
     align-items: center;
     width: 65%;
-    min-height: 250px;
+    min-height: 260px;
     margin-top: 60px;
     padding: 40px 0;
     background-size: cover !important;
@@ -180,6 +183,7 @@ export default {
 
       .event-description {
         margin-bottom: 1em;
+        white-space: pre-wrap;
       }
 
       .event-location,
