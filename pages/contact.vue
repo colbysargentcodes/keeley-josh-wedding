30dd49deba33f8794f309dbd357000d61ece083c
Colby Sargent <colby@sargent.codes>
2019-10-22 23:59:09 +0100

    update contact page content

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="page-container">
-    <h1>
+  <div class="page-container app-width">
+    <h1 class="page-header">
       Contact
     </h1>
-    <h2>
+    <h2 class="page-header">
       how to get in touch
     </h2>
     <div id="contacts-list-container">
@@ -12,20 +12,50 @@
         :key="index"
         class="contacts-list-item"
       >
+        <div class="contact-person">
+          <div class="contact-person-image">
+            <img :src="'/images/contact/' + contact.key + '.jpg'" />
+          </div>
+          <div class="contact-person-info">
+            <p class="contact-person-role text-spaced text-upper">
+              {{ contact.role }}
+            </p>
+            <p class="contact-person-name">{{ contact.name }}</p>
+          </div>
+        </div>
         <div class="contact-info">
-          <p>{{ contact.role }}</p>
-          <p>{{ contact.name }}</p>
-          <p>{{ contact.phone }}</p>
-          <p>{{ contact.email }}</p>
+          <p>
+            <a :href="'tel:' + contact.phone">{{ contact.phone }}</a>
+          </p>
+          <p>
+            <a :href="'mailto:' + contact.email">{{ contact.email }}</a>
+          </p>
         </div>
       </div>
     </div>
     <div id="emergency-container">
       <h2>Emergency!</h2>
-      <h3>112</h3>
-      <p>Police: 100</p>
-      <p>Fire: 199</p>
-      <p>Medical: 166</p>
+      <h1><a href="tel:112">112</a></h1>
+      <div id="emergency-numbers">
+        <div class="emergency-number">
+          <div class="emergency-number-name text-upper">Police</div>
+          <div class="emergency-number-number">
+            <h2><a href="tel:100">100</a></h2>
+          </div>
+        </div>
+        <div class="emergency-number">
+          <div class="emergency-number-name text-upper">Fire</div>
+          <div class="emergency-number-number">
+            <h2><a href="tel:199">199</a></h2>
+          </div>
+        </div>
+        <div class="emergency-number">
+          <div class="emergency-number-name text-upper">Medical</div>
+          <div class="emergency-number-number">
+            <h2><a href="tel:166">166</a></h2>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,19 +66,102 @@ export default {
     return {
       contacts: [
         {
-          role: 'Role 1',
-          name: 'Name 1',
-          phone: '07111 111111',
-          email: 'email-one@example.com'
+          key: 'keeley',
+          role: 'Bride',
+          name: 'Keeley',
+          phone: '+852 9686 1722',
+          email: 'keeley@keeley.com'
         },
         {
-          role: 'Role 2',
-          name: 'Name 2',
-          phone: '07222 222222',
-          email: 'email-two@example.com'
+          key: 'josh',
+          role: 'Groom',
+          name: 'Josh',
+          phone: '+852 9132 0398',
+          email: 'josh@josh.com'
+        },
+        {
+          key: 'katerina',
+          role: 'Planner',
+          name: 'Katerina',
+          phone: '+000 0000 0000',
+          email: 'kat@kat.com'
         }
       ]
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+@import '~/assets/scss/variables.scss';
+
+#contacts-list-container {
+  width: 60%;
+  margin: 75px auto;
+  font-size: 28px;
+  text-align: left;
+
+  .contacts-list-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 1.25em;
+
+    .contact-person {
+      display: flex;
+      align-items: center;
+
+      .contact-person-info {
+        line-height: 1;
+
+        .contact-person-role {
+          font-size: 18px;
+          color: $color-midgrey;
+        }
+
+        .contact-person-name {
+          font-size: 38px;
+        }
+      }
+
+      img {
+        width: 150px;
+        height: 150px;
+        border-radius: 150px;
+        margin-right: 25px;
+      }
+    }
+
+    .contact-info {
+      min-width: 28%;
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+    }
+  }
+}
+
+#emergency-container {
+  h2 {
+    font-size: 32px;
+    margin-right: -0.125em;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  #emergency-numbers {
+    display: flex;
+    justify-content: center;
+    margin-top: 25px;
+
+    .emergency-number {
+      margin: 0 50px;
+    }
+  }
+}
+</style>
